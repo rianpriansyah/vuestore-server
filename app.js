d7812cfef44852fefc2c9c12a93116c265a42760
Rian Priansyah <rian.priansyah04@gmail.com>
2023-08-30 11:03:57 +0700

    membuat config dan model product

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+/* MongoDB Server */
+// "C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath="c:\data\db"
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((result) => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to database!", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
   res.json({
