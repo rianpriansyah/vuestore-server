fdbd221d45485f95a24e10617ed34c17493d38e9
Rian Priansyah <rian.priansyah04@gmail.com>
2023-08-30 14:52:48 +0700

    membuat endpoint untuk mendapatkan satu data product

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -12,3 +12,17 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+exports.findOne = (req, res) => {
+  Product.findOne({
+    code: req.params.id,
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error while retrieving products.",
+      });
+    });
+};
