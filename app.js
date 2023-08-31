152ee6b99e14ef4b5e992e78a8ffab127e439568
Rian Priansyah <rian.priansyah04@gmail.com>
2023-08-31 12:55:38 +0700

    menampilkan data produk dari server

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ db.mongoose
     process.exit();
   });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to vuestore-server",
